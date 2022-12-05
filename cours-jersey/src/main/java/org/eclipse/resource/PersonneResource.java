0c0a179959a81baede1ef3e45da2f7b1209f92bf
baddi youssef <baddiyoussef85@gmail.com>
2022-12-05 15:52:18 +0100

    add first example

--- cours-jersey/src/main/java/org/eclipse/resource/PersonneResource.java
+++ cours-jersey/src/main/java/org/eclipse/resource/PersonneResource.java
@@ -2,8 +2,11 @@ package org.eclipse.resource;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -18,6 +21,8 @@ import org.hibernate.Session;
 @Path("personnes")
 public class PersonneResource {
 
+	Session session = HibernateUtil.getSessionFactory().openSession();
+    PersonneDao pDao = new PersonneDao(session);
     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
@@ -25,11 +30,29 @@ public class PersonneResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_XML)
     public List<Personne> getPersonnes() {
-       Session session = HibernateUtil.getSessionFactory().openSession();
-       PersonneDao pDao = new PersonneDao(session);
+       
        return pDao.findAll();
+       
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("/{id}")
+    public Personne getPersonne(@PathParam(value="id") int id) {
+       
+       return pDao.findById(id);
+       
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_XML) 
+    @Produces(MediaType.APPLICATION_XML)
+    public Personne addPersonne(Personne p) throws Exception {
+       Integer id = pDao.save(p);
+       p.setId(id);
+       return p;
        
     }
 }
