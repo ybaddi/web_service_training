a3748f8b20c581fb7fc9e8727aa131b61abc6756
baddi youssef <baddiyoussef85@gmail.com>
2022-12-07 15:30:08 +0100

    add springboot jersey and front end

--- jersey-springboot/src/main/java/com/example/jerseyspringboot/exception/ResourceNotFoundException.java
+++ jersey-springboot/src/main/java/com/example/jerseyspringboot/exception/ResourceNotFoundException.java
@@ -1,2 +1,13 @@
-package com.example.jerseyspringboot.exception;public class ResourceNotFoundException {
+package com.example.jerseyspringboot.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
--- jersey-springboot/src/main/java/com/example/jerseyspringboot/resource/PersonneResource.java
+++ jersey-springboot/src/main/java/com/example/jerseyspringboot/resource/PersonneResource.java
@@ -2,15 +2,16 @@ package com.example.jerseyspringboot.resource;
 
 
 import com.example.jerseyspringboot.dao.PersonneRepository;
+import com.example.jerseyspringboot.exception.ResourceNotFoundException;
 import com.example.jerseyspringboot.model.Personne;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,9 +25,16 @@ public class PersonneResource {
     @GET
     @Produces("application/json")
     @Path("/personnes")
-    public List<Personne> getAll(){
+    public ResponseEntity<List<Personne>> getAll(){
         // TODO
-        return personneRepository.findAll();
+        List<Personne> personnes = personneRepository.findAll();
+//        if(personne == null){
+////                .orElseThrow(()->    {//                    new ResourceNotFoundException("User not found with id "+ id)
+//            return     ResponseEntity.notFound().build();
+//
+//        }
+//                );
+        return ResponseEntity.ok().body(personnes);
     }
 
 //     TODO : find by id
@@ -35,17 +43,49 @@ public class PersonneResource {
     @Produces("application/json")
     @Path("/personnes/{id}")
     public ResponseEntity<Personne> getById(
-            @PathParam(value="id") Long id) throws Exception{
+            @PathParam(value="id") Long id) throws ResourceNotFoundException{
         // TODO
-        Personne personne = personneRepository.findById(id)
-                .orElseThrow(()-> new Exception("User not found"));
+        Personne personne = personneRepository.findById(id).orElse(null);
+        if(personne == null){
+//                .orElseThrow(()->    {//                    new ResourceNotFoundException("User not found with id "+ id)
+            return     ResponseEntity.notFound().build();
+
+        }
+//                );
         return ResponseEntity.ok().body(personne);
     }
 //    TODO find by nom
 
 //     create personne
-
+@POST
+@Produces("application/json")
+@Path("/personnes")
+public ResponseEntity<Personne> addPersonne(Personne personne){
+    // TODO
+    Personne personne1 = personneRepository.save(personne);
+//        if(personne == null){
+////                .orElseThrow(()->    {//                    new ResourceNotFoundException("User not found with id "+ id)
+//            return     ResponseEntity.notFound().build();
+//
+//        }
+//                );
+    return ResponseEntity.ok().body(personne1);
+}
 //    update personne
 
 //    delete personne
+
+    @DELETE
+    @Produces("application/json")
+    @Path("/personnes/delete/{id}")
+    public ResponseEntity<Personne> delete(
+            @PathParam(value="id") Long id) throws ResourceNotFoundException{
+        // TODO
+        Personne personne = personneRepository.findById(id).orElse(null);
+//        if(personne != null)
+            personneRepository.delete(personne);
+
+//                );
+        return ResponseEntity.ok().build();
+    }
 }
